aed546e64d1bc779afa9fddd2733f1c796cc3a77
tonyducks <antonioE@MacBook-Air-de-Admon.local>
2016-03-11 12:07:50 -0500

    proyecto actualizado usando JUCE 4.1

--- JuceLibraryCode/modules/dRowAudio/dRowAudio.h
+++ JuceLibraryCode/modules/dRowAudio/dRowAudio.h
@@ -1,5 +1,4 @@
 // This is an auto-generated file to redirect any included
 // module headers to the correct external folder.
 
-#include "../../../../../JUCE/modules/dRowAudio/dRowAudio.h"
-
+#include "../../../../../juce-grapefruit-osx/modules/dRowAudio/dRowAudio.h"
--- JuceLibraryCode/modules/juce_cryptography/juce_cryptography.h
+++ JuceLibraryCode/modules/juce_cryptography/juce_cryptography.h
@@ -1,5 +1,4 @@
 // This is an auto-generated file to redirect any included
 // module headers to the correct external folder.
 
-#include "../../../../../JUCE/modules/juce_cryptography/juce_cryptography.h"
-
+#include "../../../../../juce-grapefruit-osx/modules/juce_cryptography/juce_cryptography.h"
